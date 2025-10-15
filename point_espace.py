140df663f8cafb67dac0e65dbd2e589785280693
Kobaya65 <kobaya65@free.fr>
2025-10-15 20:26:54 +0200

    Instance arguments type changed from int to float

--- point_espace.py
+++ point_espace.py
@@ -6,7 +6,7 @@ class point_plan(object):
     """This class allows you to store the coordinates of a point,
     and also to calculate the distance to this point from another coordinate.
     """
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: float, y: float) -> None:
         self.x_a = x
         self.y_a = y
 
@@ -24,13 +24,13 @@ class point_plan(object):
         """
         print(self.__repr__())
 
-    def calcul_distance(self, x: int, y: int) -> float:
+    def calcul_distance(self, x: float, y: float) -> float:
         """Calculates distance between (x, y) and the original point.
         Args:
-            x (int): x position
-            y (int): y position
+            x (float): x position
+            y (float): y position
         Returns:
-            float: _description_
+            float: distance between original point and coordinate passed as argument
         """
         result = 0
         try:
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     distance_1 = point.calcul_distance(x_1, y_1)
     print(f"Distance entre le point d'origine {point} et <x={x_1}, y={y_1}> : {distance_1}")
 
-    x_2 = 8
-    y_2 = -4
+    x_2 = 8.45
+    y_2 = -4.639
     distance_2 = point.calcul_distance(x_2, y_2)
     print(f"Distance entre le point d'origine {point} et <x={x_2}, y={y_2}> : {distance_2}")
